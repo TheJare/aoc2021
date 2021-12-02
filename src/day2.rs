f1889b8862cfee5c2d4dbbc0e4f0a30a00c30dde
Javier Arevalo <jare@iguanademos.com>
2021-12-02 23:56:38 +0100

    flatten + transforming = flat_map

--- src/day2.rs
+++ src/day2.rs
@@ -1,24 +1,21 @@
-use crate::utils::read_from_file;
+use crate::utils::read_file;
 use anyhow::Result;
 use itertools::Itertools;
 
 // https://adventofcode.com/2021/day/2
 
 pub fn day2(args: &crate::File) -> Result<()> {
-    let (horizontal_pos, depth, aim) =
-        read_from_file(&args.file)?
-            .flatten()
-            .fold((0, 0, 0), |acc, line| {
-                if let Some((command, amount)) = line.split_whitespace().next_tuple() {
-                    return match (command, amount.parse::<i32>()) {
-                        ("down", Ok(v)) => (acc.0, acc.1, acc.2 + v),
-                        ("up", Ok(v)) => (acc.0, acc.1, acc.2 - v),
-                        ("forward", Ok(v)) => (acc.0 + v, acc.1 + acc.2 * v, acc.2),
-                        _ => acc,
-                    };
-                }
-                acc
-            });
+    let (horizontal_pos, depth, aim) = read_file(&args.file)?
+        .lines()
+        .flat_map(|line| line.split_whitespace().next_tuple())
+        .fold((0, 0, 0), |acc, (command, amount)| {
+            match (command, amount.parse::<i32>()) {
+                ("down", Ok(v)) => (acc.0, acc.1, acc.2 + v),
+                ("up", Ok(v)) => (acc.0, acc.1, acc.2 - v),
+                ("forward", Ok(v)) => (acc.0 + v, acc.1 + acc.2 * v, acc.2),
+                _ => acc,
+            }
+        });
     println!("Result of Part 1 is {}", horizontal_pos * aim);
     println!("Result of Part 2 is {}", horizontal_pos * depth);
 
