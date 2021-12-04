ebc3b9e87eabe124caf722855a8d1c5651991036
Javier Arevalo <jare@iguanademos.com>
2021-12-04 01:38:10 +0100

    Final touches

--- src/day3.rs
+++ src/day3.rs
@@ -4,15 +4,11 @@ use itertools::Itertools;
 
 // https://adventofcode.com/2021/day/3
 
-fn find_common_bit(mask: u64, values: &Vec<u64>) -> u64 {
-    let bit_balance: i32 = values
+fn find_most_common_bit(mask: u64, values: &Vec<u64>) -> u64 {
+    let bit_balance = values
         .iter()
         .fold(0, |acc, num| acc + (num & mask != 0) as i32 * 2 - 1);
     mask * (bit_balance >= 0) as u64
-}
-
-fn find_common_bits(width: usize, values: &Vec<u64>) -> u64 {
-    (0..width).fold(0, |acc, i| acc | find_common_bit(1u64 << i, values))
 }
 
 fn find_unique_value(width: usize, mut values: Vec<u64>, comparator: u64) -> u64 {
@@ -21,7 +17,7 @@ fn find_unique_value(width: usize, mut values: Vec<u64>, comparator: u64) -> u64
             break;
         }
         let cur_mask = 1u64 << bit;
-        let most_common_bit = find_common_bit(cur_mask, &values) ^ comparator;
+        let most_common_bit = find_most_common_bit(cur_mask, &values) ^ comparator;
 
         values.retain(|v| ((v ^ most_common_bit) & cur_mask) == 0);
     }
@@ -36,7 +32,7 @@ pub fn day3(args: &crate::File) -> Result<()> {
     let mask = values.iter().fold(0, |acc, num| acc | num);
     let width = (0..64).find(|i| 1 << i > mask).unwrap_or(0);
 
-    let gamma_rate = find_common_bits(width, &values);
+    let gamma_rate = (0..width).fold(0, |acc, i| acc | find_most_common_bit(1u64 << i, &values));
     let epsilon_rate = (1u64 << width) - 1 - gamma_rate;
     println!("Result of Part 1 is {}", gamma_rate * epsilon_rate);
 
