8f44f5d20bd4046482c66d9dfa23a64db56ffd7a
Javier Arevalo <jare@iguanademos.com>
2021-12-05 10:26:56 +0100

    Reuse floor from step 1 for step 2, because optimization

--- src/day5.rs
+++ src/day5.rs
@@ -14,14 +14,13 @@ pub fn read_input(args: &crate::File) -> Result<Vec<i32>> {
     Ok(tokens)
 }
 
-fn run_step(vents: &Vec<i32>, allow_diagonals: bool) -> usize {
-    let mut floor = HashMap::<(i32, i32), usize>::new();
+fn run_step(floor: &mut HashMap<(i32, i32), usize>, vents: &Vec<i32>, diagonals: bool) -> usize {
     for (&x0, &y0, &x1, &y1) in vents.into_iter().tuples() {
         let (dx, dy) = (x1 - x0, y1 - y0);
         let r = dx.abs().max(dy.abs());
         let (dx, dy) = (dx.signum(), dy.signum()); // could divide by r to do any-slope DDA
 
-        if dy == 0 || dx == 0 || allow_diagonals {
+        if (!diagonals && (dy == 0 || dx == 0)) || (diagonals && dx != 0 && dy != 0) {
             (0..=r).for_each(|i| {
                 let pos = (x0 + dx * i, y0 + dy * i);
                 floor.insert(pos, floor.get(&pos).unwrap_or(&0) + 1);
@@ -32,7 +31,11 @@ fn run_step(vents: &Vec<i32>, allow_diagonals: bool) -> usize {
 }
 
 pub fn run(vents: Vec<i32>) -> (usize, usize) {
-    (run_step(&vents, false), run_step(&vents, true))
+    let mut floor = HashMap::<(i32, i32), usize>::new();
+    (
+        run_step(&mut floor, &vents, false),
+        run_step(&mut floor, &vents, true),
+    )
 }
 
 pub fn day5(args: &crate::File) -> Result<()> {
