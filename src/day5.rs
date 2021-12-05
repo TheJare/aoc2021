c724c5f1fc8ddcc2030a5cd56b63aa6033e6f1e8
Javier Arevalo <jare@iguanademos.com>
2021-12-05 21:46:36 +0100

    Switch from hashmap to vector, 13ms -> 4ms

--- src/day5.rs
+++ src/day5.rs
@@ -12,10 +12,10 @@ pub fn read_input(args: &crate::File) -> Result<(Vec<i32>, i32)> {
         .flat_map(|v| v.parse())
         .collect_vec();
     let &max = tokens.iter().max().unwrap_or(&0);
-    Ok((tokens, max))
+    Ok((tokens, max + 1))
 }
 
-fn run_step(floor: &mut HashMap<i32, usize>, vents: &Vec<i32>, max: i32, diagonals: bool) -> usize {
+fn run_step(floor: &mut Vec<usize>, vents: &Vec<i32>, max: i32, diagonals: bool) -> usize {
     let mut acc = 0;
     for (&x0, &y0, &x1, &y1) in vents.into_iter().tuples() {
         let (dx, dy) = (x1 - x0, y1 - y0);
@@ -24,16 +24,10 @@ fn run_step(floor: &mut HashMap<i32, usize>, vents: &Vec<i32>, max: i32, diagona
 
         if (!diagonals && (dy == 0 || dx == 0)) || (diagonals && dx != 0 && dy != 0) {
             (0..=r).for_each(|i| {
-                let pos = (x0 + dx * i) + max * (y0 + dy * i);
-                match floor.get_mut(&pos) {
-                    Some(count) => {
-                        *count = *count + 1;
-                        acc = acc + (*count == 2) as usize;
-                    }
-                    None => {
-                        floor.insert(pos, 1);
-                    }
-                }
+                let pos = ((x0 + dx * i) + max * (y0 + dy * i)) as usize;
+                let count = floor[pos] + 1;
+                floor[pos] = count;
+                acc = acc + (count == 2) as usize;
             });
         }
     }
@@ -41,7 +35,7 @@ fn run_step(floor: &mut HashMap<i32, usize>, vents: &Vec<i32>, max: i32, diagona
 }
 
 pub fn run(vents: Vec<i32>, max: i32) -> (usize, usize) {
-    let mut floor = HashMap::<i32, usize>::new();
+    let mut floor = vec![0usize; (max * max) as usize];
     let step1 = run_step(&mut floor, &vents, max, false);
     (step1, step1 + run_step(&mut floor, &vents, max, true))
 }
@@ -49,7 +43,7 @@ pub fn run(vents: Vec<i32>, max: i32) -> (usize, usize) {
 pub fn day5(args: &crate::File) -> Result<()> {
     let (vents, max) = read_input(&args)?;
 
-    let result = run(vents, max + 1);
+    let result = run(vents, max);
 
     println!("Result of Part 1 is {}", result.0);
     println!("Result of Part 2 is {}", result.1);
