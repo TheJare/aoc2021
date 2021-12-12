bfec97597a15d28df2ad1c073da2a8018e0e42aa
Javier Arevalo <jare@iguanademos.com>
2021-12-12 23:53:59 +0100

    Bit scanning -> explicit vec of edge targets : 3ms

--- src/day12.rs
+++ src/day12.rs
@@ -6,7 +6,7 @@ use std::cmp::Ordering;
 // https://adventofcode.com/2021/day/12
 
 #[derive(Debug)]
-pub struct Map(Vec<u32>, usize, usize, usize);
+pub struct Map(Vec<Vec<usize>>, usize, usize, usize);
 
 pub fn read_input(args: &crate::File) -> Result<Map> {
     let file = read_file(&args.file)?;
@@ -22,18 +22,18 @@ pub fn read_input(args: &crate::File) -> Result<Map> {
         .ok()
         .context("can't find end node")?;
     let smallcave_min_index = nodes.partition_point(|&s| s.cmp("a") == Ordering::Less);
-    let mut map = vec![0u32; nodes.len()];
+    let mut map = vec![Vec::<usize>::new(); nodes.len()];
     for line in file.split_ascii_whitespace() {
         if let Some((a, b)) = line.split('-').collect_tuple() {
             let (a, b) = (
                 nodes.binary_search(&a.trim()).unwrap(),
                 nodes.binary_search(&b.trim()).unwrap(),
             );
-            if b != start {
-                map[a] |= 1 << b;
+            if b != start && !map[a].contains(&b) {
+                map[a].push(b);
             }
-            if a != start {
-                map[b] |= 1 << a;
+            if a != start && !map[b].contains(&a) {
+                map[b].push(a);
             }
         }
     }
@@ -43,23 +43,18 @@ pub fn read_input(args: &crate::File) -> Result<Map> {
 fn advance(map: &Map, node: usize, visited: u32, long_path: bool) -> (usize, usize) {
     let (mut r1, mut r2) = (0, 0);
 
-    let targets = map.0[node];
-    for target in 0..map.0.len() {
+    let targets = &map.0[node];
+    for &target in targets.iter() {
         let target_bit = 1 << target;
-        if target_bit > targets {
-            break;
-        }
-        if targets & target_bit != 0 {
-            if target == map.2 {
-                r1 += !long_path as usize;
-                r2 += 1;
-            } else {
-                let is_visited = target >= map.3 && visited & target_bit != 0;
-                if !long_path || !is_visited {
-                    let r = advance(map, target, visited | target_bit, long_path || is_visited);
-                    r1 += r.0;
-                    r2 += r.1;
-                }
+        if target == map.2 {
+            r1 += !long_path as usize;
+            r2 += 1;
+        } else {
+            let is_visited = target >= map.3 && visited & target_bit != 0;
+            if !long_path || !is_visited {
+                let r = advance(map, target, visited | target_bit, long_path || is_visited);
+                r1 += r.0;
+                r2 += r.1;
             }
         }
     }
